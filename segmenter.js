67f55b784055eda8854bae3415ee479227bed05e
Adrian Shum <me@cshum.com>
2015-09-15 17:26:52 +0800

    progressing

--- segmenter.js
+++ segmenter.js
@@ -2,14 +2,19 @@ var jieba = require('nodejieba')
 var ginga = require('ginga')
 var H = require('highland')
 
+var UNI = /([^\x00-\xFF]){1,}/g
 var AFTER = /\:.*$/
+
+function clean (token) {
+  return token.replace(AFTER, '')
+}
 
 module.exports = function segmenter () {
   return ginga().use('pipeline', function (ctx) {
-    ctx.tokens = H(ctx.tokens).flatMap(function (token) {
-      return H(jieba.extract(token, -1)).map(function (token) {
-        return token.replace(AFTER, '')
-      })
+    ctx.tokens = H(ctx.tokens)
+    .flatMap(function (token) {
+      console.log(token.match(UNI))
+      return H(jieba.extract(token, -1)).map(clean)
     })
   })
 }
--- test/test.js
+++ test/test.js
@@ -1,25 +1,68 @@
 var test = require('tape')
 var levi = require('levi')
 var chinese = require('../')
+var ginga = require('ginga')
 
 var lv = levi('db', {db: require('memdown')})
 .use(levi.tokenizer())
-.use(chinese.converter())
-.use(chinese.segmenter())
 .use(levi.stemmer())
 .use(levi.stopword())
+.use(chinese.converter())
+.use(chinese.segmenter())
 
-test('pipeline', function (t) {
-  lv.pipeline('Re-thinking the JavaScript utility belt, 典載入方式靈活，無需配置詞典路徑也可使用，需要定制自己的詞典路徑時也可靈活定制', function (err, tokens) {
-    t.notOk(err)
-    t.deepEqual(tokens, [
-      'includ', 'foo', 'abc', 'instanceof', 'constructor'
-    ], 'stemmer, stopwords, js reserved words')
-    lv.pipeline(tokens, function (err, tokens2) {
+test('Jieba Segmenter', function (t) {
+  lv.pipeline(
+    '我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。',
+    function (err, tokens) {
       t.notOk(err)
-      t.deepEqual(tokens, tokens2, 'idempotent')
-      t.end()
-    })
-  })
+      t.deepEqual(tokens, [
+        'ceo', '升职', '加薪', '手扶拖拉机', '巅峰', '拖拉机', '当上',
+        '多久', '走上', '人生', '学院', '专业', '不用'
+      ])
+      lv.pipeline(tokens, function (err, tokens2) {
+        t.notOk(err)
+        t.deepEqual(tokens, tokens2, 'idempotent')
+        t.end()
+      })
+    }
+  )
 })
 
+test('Chinese converter', function (t) {
+  lv.pipeline(
+    '我是拖拉機學院手扶拖拉機專業的。不用多久，我就會升職加薪，當上CEO，走上人生巔峰。',
+    function (err, tokens) {
+      t.notOk(err)
+      t.deepEqual(tokens, [
+        'ceo', '升职', '加薪', '手扶拖拉机', '巅峰', '拖拉机', '当上',
+        '多久', '走上', '人生', '学院', '专业', '不用'
+      ])
+      lv.pipeline(tokens, function (err, tokens2) {
+        t.notOk(err)
+        t.deepEqual(tokens, tokens2, 'idempotent')
+        t.end()
+      })
+    }
+  )
+})
+
+test('Mixed', function (t) {
+  lv.pipeline(
+    '我是拖拉機學院手扶拖拉機專業的。'+
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '+
+    '不用多久，我就會升職加薪，當上CEO，走上人生巔峰。'+
+    'Lorem Ipsum has been the industry standard dummy text ever since the 1500s',
+    function (err, tokens) {
+      t.notOk(err)
+      t.deepEqual(tokens, [
+        'ceo', '升职', '加薪', '手扶拖拉机', '巅峰', '拖拉机', '当上',
+        '多久', '走上', '人生', '学院', '专业', '不用'
+      ])
+      lv.pipeline(tokens, function (err, tokens2) {
+        t.notOk(err)
+        t.deepEqual(tokens, tokens2, 'idempotent')
+        t.end()
+      })
+    }
+  )
+})
