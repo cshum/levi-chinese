33af47ad3f2b088e542c32a67465f559a68f5158
Adrian Shum <me@cshum.com>
2015-09-19 10:57:40 +0800

    converter clean up

--- converter.js
+++ converter.js
@@ -1,12 +1,11 @@
 var ginga = require('ginga')
 var H = require('highland')
-var right = require('./s2t.json')
-var wrong = require('./t2s.json')
+var t2s = require('./t2s.json')
 var UNI = /[^\x00-\xFF]/g
 
 // traditional/simplified chinese conversion
-module.exports = function converter (trad) {
-  var map = trad ? right : wrong
+module.exports = function converter (map) {
+  map = map || t2s
 
   function replaceFn (s) {
     return (s in map) ? map[s] : s
